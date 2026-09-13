beb7cab3c966c28045b63c1dba6f55e508c4b9fc
TJ <insrayong@gmail.com>
2026-09-13 16:23:09 +0700

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 // สามารถระบุค่าเริ่มต้นตรงนี้ หรือจะตั้งค่าผ่านปุ่ม "⚙️ ตั้งค่า Supabase" บนหน้าเว็บก็ได้ (จะจำใน localStorage)
 window.SUPABASE_CONFIG = {
     // เช่น: 'https://xxxxxxxxxxxxxxxxxxxx.supabase.co'
-    url: '',
+    url: 'https://frbyowazvgveavskgbrr.supabase.co',
     // เช่น: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
-    anonKey: ''
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyYnlvd2F6dmd2ZWF2c2tnYnJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxOTUyNDIsImV4cCI6MjEwNDc3MTI0Mn0.AcracXzxM5MNdB-1VJ5cJ4fdNBI5WMP4LeH49tIZLss'
 };
