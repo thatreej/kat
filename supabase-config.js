61459e17a07d3247483789b8847c3be075eba1f0
TJ <insrayong@gmail.com>
2026-09-13 16:18:18 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+// การตั้งค่า Supabase สำหรับระบบติดตามระยะทางรถ
+// สามารถระบุค่าเริ่มต้นตรงนี้ หรือจะตั้งค่าผ่านปุ่ม "⚙️ ตั้งค่า Supabase" บนหน้าเว็บก็ได้ (จะจำใน localStorage)
+window.SUPABASE_CONFIG = {
+    // เช่น: 'https://xxxxxxxxxxxxxxxxxxxx.supabase.co'
+    url: '',
+    // เช่น: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+    anonKey: ''
+};
